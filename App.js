739c0454572dc7213b1d4381170090bfb71953f3
Md Najmul Haque <najmulhaque008@gmail.com>
2023-05-23 16:20:34 +0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, Text, View } from 'react-native';
+import { Logo } from "./assets/logo.png"
+import { Logo2 } from "./assets/2.png"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1  bg-white">
+      <View className="mt-5">
+        <Image source={{ uri: 'https://i.ibb.co/gtB5sR9/logo.png' }}
+          className="w-76 h-32" />
+        <Text>Welcome to Revinns Ltd</Text>
+        <View className='flex-row justify-center'>
+          <Button
+            onPress={() => { }}
+            title="Sing In"
+            className=""
+          />
+          <Button
+            onPress={() => { }}
+            title="Sign Up"
+            className="bg-warning ml-3"
+          />
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
