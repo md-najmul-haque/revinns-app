8d092b52bf07b264eb04779e0867bbaafd317855
Md Najmul Haque <najmulhaque008@gmail.com>
2023-06-10 12:35:19 +0600

    login page updagted

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,6 +1,7 @@
 import { Button, Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import React, { useState } from 'react'
 import Toast from 'react-native-toast-message';
+import { Avatar, Caption } from "react-native-paper"
 
 const SignIn = ({ navigation }) => {
 
@@ -49,44 +50,47 @@ const SignIn = ({ navigation }) => {
 
 
     return (
-        <SafeAreaView className="h-screen bg-white">
-            <View className='mx-5 mt-10'>
-                <Image source={require('../assets/logo.jpg')} className="w-76 h-32" />
+        <SafeAreaView className="flex-1 justify-center items-center h-screen bg-white">
+            <View className='mx-5'>
+                <View className='flex-row justify-center items-center mt-2'>
+                    <Avatar.Image size={40} className="" source={require('../assets/logo.jpg')} />
+                    <Text className='text-2xl text-primary font-semibold text-center ml-2'>Bangladesh Multicare Hospital</Text>
+                </View>
 
                 <View className='mt-5'>
 
-                    <View>
-                        <Text className='text-2xl mb-5 text-primary font-semibold'>Welcome Back!</Text>
+
+                    <Text className='text-2xl mb-5 text-primary text-center font-semibold'>Welcome Back!</Text>
 
 
-                        <TextInput
-                            className='bg-white border border-gray-300 h-10 rounded-md p-2 mb-4 min-w-full'
-                            placeholder="User Name"
-                            onChangeText={text => setUserName(text)}
-                        />
-                        <TextInput
-                            className='bg-white border border-gray-300 h-10 rounded-md p-2 mb-4 min-w-full'
-                            placeholder="Password"
-                            secureTextEntry
-                            onChangeText={text => setPassword(text)}
-                        />
+                    <TextInput
+                        className='bg-white border border-gray-300 h-10 rounded-md p-2 mb-4 min-w-full'
+                        placeholder="User Name"
+                        onChangeText={text => setUserName(text)}
+                    />
+                    <TextInput
+                        className='bg-white border border-gray-300 h-10 rounded-md p-2 mb-4 min-w-full'
+                        placeholder="Password"
+                        secureTextEntry
+                        onChangeText={text => setPassword(text)}
+                    />
 
-                        <View style={styles.checkboxContainer}>
-                            <TouchableOpacity style={styles.checkbox} onPress={toggleRememberMe}>
-                                {rememberMe ? (
-                                    <Text style={styles.checkboxText}>✓</Text>
-                                ) : (
-                                    <Text style={styles.checkboxText}></Text>
-                                )}
-                            </TouchableOpacity>
-                            <Text style={styles.label}>Remember Me</Text>
-                        </View>
-
-                        <Button title="Login" color='#ff6400' onPress={handleLogin} />
-
-                        <Text className='mt-5 text-center'>Don't have an account? <Text className='text-secondary font-semibold' onPress={() => navigation.navigate('SignUp')}>Sign Up</Text> </Text>
-
+                    <View style={styles.checkboxContainer}>
+                        <TouchableOpacity style={styles.checkbox} onPress={toggleRememberMe}>
+                            {rememberMe ? (
+                                <Text style={styles.checkboxText}>✓</Text>
+                            ) : (
+                                <Text style={styles.checkboxText}></Text>
+                            )}
+                        </TouchableOpacity>
+                        <Text style={styles.label}>Remember Me</Text>
                     </View>
+
+                    <Button title="Login" color='#ff6400' onPress={handleLogin} />
+
+                    <Text className='mt-5 text-center'>Don't have an account? <Text className='text-secondary font-semibold' onPress={() => navigation.navigate('SignUp')}>Sign Up</Text> </Text>
+
+
 
                 </View>
 
